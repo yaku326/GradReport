b33de564b7be9dc0d320aae28d5d8baa0306cf98
ioriveur(iori-yja) <fivo.11235813@gmail.com>
2011-03-14 12:28:01 +0900

    Mar 14 check release.

---
+++ textest/images.pl
@@ -0,0 +1,147 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/SonitusoverLucistimesColorum;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|3#3|; 
+
+$key = q/overline{BHE};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|11#11|; 
+
+$key = q/{lstlisting}alwaysbegin#576HREF<=1;#2560HREF<=0;endalwaysbegin#1589952VSYNC<=1;#9408VSYNC<=0;end{lstlisting};AAT/;
+$cached_env_img{$key} = q|27#27|; 
+
+$key = q/{lstlisting}[caption=ラベルLC3].LC3:.ascii”Data%4x{backslash{012{backslash{000″.space3{lstlisting};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|40#40|; 
+
+$key = q/{lstlisting}moduletransmittr(inputSrialclk,inputWrtcmplt,input[11:0]data,outputSginEndflg<=0;Datareg[11:0]<=data[11:0];endend{par{endmodule{par{{lstlisting};AAT/;
+$cached_env_img{$key} = q|22#22|; 
+
+$key = q/{lstlisting}[caption=printf()呼び出し部]ldrr0,.L71+4movr1,r4blprintf{lstlisting};AAT/;
+$cached_env_img{$key} = q|44#44|; 
+
+$key = q/includegraphics[width=0.7hsize]{fig3.eps};AAT/;
+$cached_env_img{$key} = q|25#25|; 
+
+$key = q/{lstlisting}[caption=子ルーチン].LVL64:.loc12640movip,#0.loc12650movr0,ipmovr1,#66movr2,#8.loc12640strip,[r4,#0].loc12650bli2csender{lstlisting};AAT/;
+$cached_env_img{$key} = q|34#34|; 
+
+$key = q/{lstlisting}SCCBByteWrite(0x1204);vTaskDelay(2slashportTICK_RATE_MS);SCCBByteWri);vTaskDelay(2slashportTICK_RATE_MS);SCCBByteWrite(0x8C02);:::::{lstlisting};AAT/;
+$cached_env_img{$key} = q|30#30|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN2011.JPG};AAT/;
+$cached_env_img{$key} = q|46#46|; 
+
+$key = q/SCCB;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|1#1|; 
+
+$key = q/{lstlisting}[caption=孫ルーチン:C言語]voidi2csender(intContinue,unsignedintData,while(I22STAT!=0×28);printf(“Data%4xn”,Data);Data=Data>>8;}}}{lstlisting};AAT/;
+$cached_env_img{$key} = q|36#36|; 
+
+$key = q/I^2C;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|2#2|; 
+
+$key = q/{lstlisting}Hello,world!1:0203PDIRRegisterResetSetupbegin[======================================>{lstlisting};AAT/;
+$cached_env_img{$key} = q|33#33|; 
+
+$key = q/{lstlisting}[caption=OV7670userconfig.hの抜粋]constintOV7670[]={0x1204,0x40D0,0x8C02,0x703a,0x7211,::::{lstlisting};AAT/;
+$cached_env_img{$key} = q|32#32|; 
+
+$key = q/{lstlisting}[caption=子ルーチンさらに抜粋].LVL64:.loc12640movip,#0.loc12650movr0つめmovr2,#8@引数3つめ.loc12640strip,[r4,#0].loc12650bli2csender{lstlisting};AAT/;
+$cached_env_img{$key} = q|37#37|; 
+
+$key = q/{lstlisting}[caption=孫ルーチン]i2csender:.LFB5:.loc11340.cfi_startproc@Functionrr3,[r5,#4]cmpr3,#40bne.L63.loc11520ldrr0,.L71+4movr1,r4blprintf{lstlisting};AAT/;
+$cached_env_img{$key} = q|35#35|; 
+
+$key = q/includegraphics[width=hsize]{tmchart.eps};AAT/;
+$cached_env_img{$key} = q|18#18|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN2010.JPG};AAT/;
+$cached_env_img{$key} = q|28#28|; 
+
+$key = q/backslash;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|42#42|; 
+
+$key = q/{lstlisting}[caption=データコミッタ]moduleDtacutcmmit(inputSigEn,input[7:0]pshdtgeSigEn)beginpopdta[11:0]<={redpx[3:0],pshdta[7:0]};endendmodule{lstlisting};AAT/;
+$cached_env_img{$key} = q|21#21|; 
+
+$key = q/overline{CE};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|8#8|; 
+
+$key = q/{lstlisting}[caption=回路検証のためのモジュール.実回路には組み込まれない]moduleTways@pixbus{display({time,"pix:<comment_mark>24endmodule{lstlisting};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|26#26|; 
+
+$key = q/{lstlisting}[caption=特定のピクセルのサイクルのときに立ち上がる、一部省略]modulen<=1'b1;foo<=7'b0000000;endelsebeginSig_En<=1'b0;endendendmodule{lstlisting};AAT/;
+$cached_env_img{$key} = q|17#17|; 
+
+$key = q/アドレス長times1アドレスに於けるデータ幅=veticaltimeshorizonaltimes2times8bit;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|6#6|; 
+
+$key = q/アドレス長times1アドレスに於けるデータ幅=veticaltimeshorizonaltimes2times16bit;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|7#7|; 
+
+$key = q/overline{WE};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|9#9|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN2014.JPG};AAT/;
+$cached_env_img{$key} = q|47#47|; 
+
+$key = q/{lstlisting}[caption=ラベルLC3].LC3:.ascii“Data%4x::::1200″.space3.L71:.word-536346624.word.LC3.word.LC1.word.LC2.wordi2cErr.cfi_endproc{lstlisting};AAT/;
+$cached_env_img{$key} = q|43#43|; 
+
+$key = q/{lstlisting}[caption=分周器]moduleclkdivider(inputclk,outputxclk);reg[1:0]t_councount[0])t_count[1]<=~t_count[1];assignxclk=t_count[1];endmodule{lstlisting};AAT/;
+$cached_env_img{$key} = q|16#16|; 
+
+$key = q/{lstlisting}[caption=一気にシミュレーションまで行う1行スクリプト]vsimwork.TEST;addwave-rslash*;run1600000;{lstlisting};AAT/;
+$cached_env_img{$key} = q|23#23|; 
+
+$key = q/{lstlisting}[caption=printf呼び出し:アセンブリ].LCFI1:.cfi_def_cfa_offset24.loc1rr0,.L71+4ここが問題のprintf()にたいする引数渡しmovr1,r4blprintf{lstlisting};AAT/;
+$cached_env_img{$key} = q|38#38|; 
+
+$key = q/{lstlisting}[caption=ピクセルごとのカウンタ]always@(posedgepclk)beginif(HREF&enCeginSig_En<=1'b1;foo<=7'b0000000;endelsebeginSig_En<=1'b0;endend{lstlisting};AAT/;
+$cached_env_img{$key} = q|19#19|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN2012.JPG};AAT/;
+$cached_env_img{$key} = q|5#5|; 
+
+$key = q/{lstlisting}[caption=ラベルL71].L71:.word-536346624.word.LC3.word.LC1.word.LC2.wordi2cErr.cfi_endproc{lstlisting};AAT/;
+$cached_env_img{$key} = q|39#39|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN1987.JPG};AAT/;
+$cached_env_img{$key} = q|15#15|; 
+
+$key = q/{lstlisting}[caption=ov7670_set_up.ttl];OV7670<81>@setupmacro;;delaysetpause_ms=mpausepause_mssendln'W1500'sendlnmpausepause_mssendln'W7A20'::::{lstlisting};AAT/;
+$cached_env_img{$key} = q|29#29|; 
+
+$key = q/includegraphics[scale=0.7,bb=1020600400,clip]{fig1.eps};AAT/;
+$cached_env_img{$key} = q|14#14|; 
+
+$key = q/includegraphics[width=0.7hsize]{fig2.eps};AAT/;
+$cached_env_img{$key} = q|24#24|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN2006.JPG};AAT/;
+$cached_env_img{$key} = q|13#13|; 
+
+$key = q/overline{BLE};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|10#10|; 
+
+$key = q/includegraphics[scale=0.25,bb=922025001400,clip]{DSCN2008.JPG};AAT/;
+$cached_env_img{$key} = q|4#4|; 
+
+$key = q/{lstlisting}[caption=行のカウンタ]always@(posedgeHREForposedgeVSYNC)beginif(HREF<=line+1'b1;elseline=8'h00;endassignenC=(line==9'h30)?1'b1:1'b0;{lstlisting};AAT/;
+$cached_env_img{$key} = q|20#20|; 
+
+$key = q/{lstlisting}[caption=i2c.cのvi2cタスクの冒頭]printf("Setupbegin[>");fflush(stdourtTICK_RATE_MS);{printf("rDatatransmitionEnd!n");fflush(stdout);{lstlisting};AAT/;
+$cached_env_img{$key} = q|31#31|; 
+
+$key = q/overline{OE};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|12#12|; 
+
+$key = q/https:slashslashdl.dropbox.comslashuslash15570814slash051020DDI0100HJ_v6_1.pdf;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|41#41|; 
+
+$key = q/{lstlisting}[caption=objdumpで見たprintf()呼び出し部]3c:e59f009cldrr0,[pc,#156];e1a01004movr1,r444:ebfffffebl0<printf>e0:0000003c.word0x0000003c{lstlisting};AAT/;
+$cached_env_img{$key} = q|45#45|; 
+
+1;
+
